38c44faf51cbdb911a04fa17182a8f729dfa04ee
abdelrahmangaber528 <abdelrahmangaber528@gmail.com>
2025-10-31 07:35:33 +0200

    Change main file_name to run_full_pipeline

--- STT-NLP/run_full_pipeline.py
+++ STT-NLP/run_full_pipeline.py
@@ -1,7 +1,7 @@
 from src.pipeline.training_pipeline import TrainingPipeline
 from src import get_logger
 
-logger = get_logger("Main")
+logger = get_logger("STT-Main")
 
 if __name__ == "__main__":
     try:
