0b3d08655c3fb0e786be335b23e727d2b236216e
abdelrahmangaber528 <abdelrahmangaber528@gmail.com>
2025-11-13 20:38:27 +0200

    Final Train , i lost my brain

--- STT-NLP/predict.py
+++ STT-NLP/predict.py
@@ -1,5 +1,5 @@
 from src.pipeline.inference_pipeline import InferencePipeline
 
 model = InferencePipeline()
-text = model.predict("5639-40744-0001.wav")
+text = model.predict("5639-40744-0032.wav")
 print(text)
